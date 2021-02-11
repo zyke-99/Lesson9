250fb9a119fee2dba65b8acf9a53b2dd7955200e
zyke-99 <augustas.kersys@gmail.com>
2021-02-11 15:25:36 +0200

    Fix: fixed failing tests.

--- src/test/java/com/example/demo/MessageControllerTests.java
+++ src/test/java/com/example/demo/MessageControllerTests.java
@@ -30,11 +30,11 @@ public class MessageControllerTests {
     @Test
     public void shouldBEandDBWork() {
         when(repository.findAll()).thenReturn(Arrays.asList(new Message(MESSAGE)));
-        assertEquals(MessageController.BE_SUCCESS_MESSAGE + MessageController.DB_SUCCESS_MESSAGE + MESSAGE, controller.helloWorld());
+        assertEquals(MessageController.BE_SUCCESS_MESSAGE + MessageController.DB_SUCCESS_MESSAGE + MESSAGE, controller.getMessage());
     }
     @Test
     public void shouldWorkOnlyBE() {
         when(repository.findAll()).thenReturn(new ArrayList<>());
-        assertEquals(MessageController.BE_SUCCESS_MESSAGE, controller.helloWorld());
+        assertEquals(MessageController.BE_SUCCESS_MESSAGE, controller.getMessage());
     }
 }
